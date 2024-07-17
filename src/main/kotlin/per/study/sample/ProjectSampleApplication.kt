15f4202c7f55174dada29a8e14bc274d14987da0
ddu0422 <ddu0422@naver.com>
2024-07-17 10:23:01 +0900

    chore: init project

---
+++ src/main/kotlin/per/study/sample/ProjectSampleApplication.kt
@@ -0,0 +1,11 @@
+package per.study.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectSampleApplication>(*args)
+}
---
+++ src/test/kotlin/per/study/sample/ProjectSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package per.study.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
