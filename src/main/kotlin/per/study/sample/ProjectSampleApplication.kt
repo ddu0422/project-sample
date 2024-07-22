4a7173beecc49e4928e187c244dc6b7cca578b59
ddu0422 <ddu0422@naver.com>
2024-07-22 14:14:40 +0900

    feat: Memory 계산 출력

--- src/main/kotlin/per/study/sample/ProjectSampleApplication.kt
+++ src/main/kotlin/per/study/sample/ProjectSampleApplication.kt
@@ -3,9 +3,25 @@ package per.study.sample
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+private const val MB = 1024 * 1024
+private const val MEMORY_UNIT = "MB"
+
 @SpringBootApplication
 class ProjectSampleApplication
 
 fun main(args: Array<String>) {
+    val runtime = Runtime.getRuntime()
+
+    val maxMemory = runtime.maxMemory()
+    val freeMemory = runtime.freeMemory()
+    val allocatedMemory = runtime.totalMemory()
+
+    println("================= Memory Info =================")
+    println("Free Memory: ${freeMemory.div(MB)} $MEMORY_UNIT")
+    println("Allocated Memory: ${allocatedMemory.div(MB)} $MEMORY_UNIT")
+    println("Max Memory: ${maxMemory.div(MB)} $MEMORY_UNIT")
+    println("Total Memory: ${freeMemory + maxMemory.minus(allocatedMemory).div(MB)} $MEMORY_UNIT")
+    println("===============================================")
+
     runApplication<ProjectSampleApplication>(*args)
 }
